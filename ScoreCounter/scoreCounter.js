10f5224d05938fd3fdb2b53a66e77d76296ef9ce
Chris Hunter <chris-2k6@hotmail.co.uk>
2019-05-28 22:40:02 +0100

    Added html controls and js functionality to increment player's scores when player's button is clicked. Also prevent scores from incrementing once a player reaches a maximum score. The game can also be reset.

--- ScoreCounter/scoreCounter.js
+++ ScoreCounter/scoreCounter.js
@@ -1,1 +1,53 @@
-alert("connected");
+
+var p1Btn = document.querySelector("#p1");
+var p2Btn = document.getElementById("p2");
+var resetBtn = document.getElementById("reset");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var maxScoreDisplay = document.getElementById("maxScoreDisplay");
+
+
+// Scores
+var p1Score = 0;
+var p2Score = 0;
+var maxScore = 5;
+var gameOver = false;
+
+p1Btn.addEventListener("click", function(){ 
+	if (!gameOver && maxScore > 0){
+		p1Score++;
+		p1Display.textContent = p1Score;
+		if (p1Score === maxScore)
+		{
+			gameOver = true;
+			maxScoreLabel.textContent += "... CONGRATULATIONS PLAYER 1, YOU HAVE WON!";
+		}
+	}
+});
+
+p2Btn.addEventListener("click", function(){
+	if (!gameOver && maxScore > 0){
+		p2Score++;
+		p2Display.textContent = p2Score;
+		if (p2Score === maxScore)
+		{
+			gameOver = true;
+			maxScoreLabel.textContent += "... CONGRATULATIONS PLAYER 2, YOU HAVE WON!";
+		}
+	}
+});
+
+resetBtn.addEventListener("click", function(){
+	reset();
+});
+
+function reset() {
+	p1Score = 0;
+	p1Display.textContent = p1Score;
+	p2Score = 0;
+	p2Display.textContent = p2Score;
+	maxScoreDisplay = maxScore;
+	gameOver = false;	
+}
+
+reset();
