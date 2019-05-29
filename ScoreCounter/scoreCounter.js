1f25cfb18364bb1401dc8d8deb236a72f34e0d95
Chris Hunter <chris-2k6@hotmail.co.uk>
2019-05-29 21:54:11 +0100

    Fixed event listener for numeric input to set winning score and reset current player scores on change. Added css file to show winner's number in green and losers in red when the game is won. Also added conversion of numeric input which caused game to be never ending when comparing with player scores.

--- ScoreCounter/scoreCounter.js
+++ ScoreCounter/scoreCounter.js
@@ -1,37 +1,41 @@
 
 var p1Btn = document.querySelector("#p1");
 var p2Btn = document.getElementById("p2");
+var maxScoreInput = document.querySelector("#winningScoreInput");
 var resetBtn = document.getElementById("reset");
+
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var maxScoreDisplay = document.getElementById("maxScoreDisplay");
 
-
-// Scores
 var p1Score = 0;
 var p2Score = 0;
-var maxScore = 5;
+var maxScore = 0;
 var gameOver = false;
 
 p1Btn.addEventListener("click", function(){ 
-	if (!gameOver && maxScore > 0){
+	if (!gameOver && winningScore > 0){
 		p1Score++;
 		p1Display.textContent = p1Score;
-		if (p1Score === maxScore)
+		if (p1Score === winningScore)
 		{
 			gameOver = true;
+			p1Display.classList.add("winner");
+			p2Display.classList.add("looser");
 			maxScoreLabel.textContent += "... CONGRATULATIONS PLAYER 1, YOU HAVE WON!";
 		}
 	}
 });
 
 p2Btn.addEventListener("click", function(){
-	if (!gameOver && maxScore > 0){
+	if (!gameOver && winningScore > 0){
 		p2Score++;
 		p2Display.textContent = p2Score;
-		if (p2Score === maxScore)
+		if (p2Score === winningScore)
 		{
 			gameOver = true;
+			p2Display.classList.add("winner");
+			p1Display.classList.add("looser");
 			maxScoreLabel.textContent += "... CONGRATULATIONS PLAYER 2, YOU HAVE WON!";
 		}
 	}
@@ -41,13 +45,20 @@ resetBtn.addEventListener("click", function(){
 	reset();
 });
 
+maxScoreInput.addEventListener("change", function() {
+	winningScore = Number(this.value);
+	maxScoreDisplay.textContent = winningScore;
+	reset();
+});
+
 function reset() {
 	p1Score = 0;
-	p1Display.textContent = p1Score;
 	p2Score = 0;
-	p2Display.textContent = p2Score;
-	maxScoreDisplay = maxScore;
-	gameOver = false;	
-}
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	gameOver = false;
+	p1Display.classList = "";
+	p2Display.classList = "";
+};
 
 reset();
